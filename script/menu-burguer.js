8593832ef03c9a9e1ac7e678ed073bf270aa4698
Julio Cesar Bertoldo <juliocesar.bertt@gmail.com>
2023-09-08 10:48:28 -0300

    .

--- script/menu-burguer.js
+++ script/menu-burguer.js
@@ -1,16 +1,28 @@
 function mudouTamanho() {
     if (window.innerWidth >= 768) {
-        itensMenu.style.display = 'block'
+        escureceMenuAtivo.style.display = 'none'
+        containerConteudoSaibaMais.style.display = 'block'
     } else {
-        itensMenu.style.display = 'none'
+        escureceMenuAtivo.style.display = 'none'
+        containerConteudoSaibaMais.style.display = 'none'
+        saibaMais.style.visibility = 'visible'
+        itensMenu.style.right = '-260px'
     }
 }
 
 function clickMenu() {
-    if (itensMenu.style.display == 'block') {
-        itensMenu.style.display = 'none'
+    if (itensMenu.style.right == '0px') {
+        itensMenu.style.right = '-260px'
     } else {
-        itensMenu.style.display = 'block'
+        escureceMenuAtivo.style.display = 'block'
+        itensMenu.style.right = '0px'
+        containerConteudoSaibaMais.style.display = 'none'
+        saibaMais.style.visibility = 'visible'
     }
+}
+
+function clickFechaMenu() {
+    itensMenu.style.right = '-260px'
+    escureceMenuAtivo.style.display = 'none'
 }
 
---
+++ script/saiba-mais.js
@@ -0,0 +1,9 @@
+function clickSaibaMais() {
+    containerConteudoSaibaMais.style.display = 'block'
+    saibaMais.style.visibility = 'hidden'
+}
+
+function clickEsconderSaibaMais() {
+    containerConteudoSaibaMais.style.display = 'none'
+    saibaMais.style.visibility = 'visible'
+}
